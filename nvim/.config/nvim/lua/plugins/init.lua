909e75c02c2a82d48cb0072bb61118ab51807177
Jeremiah Griffin <jeremiah@griffin.to>
2024-01-25 17:46:37 -0800

    nvim: Enable multi-file opening through Telescope

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -94,7 +94,29 @@ return {
       },
     },
     config = function()
-      require('telescope').setup({})
+      local select_one_or_multi = function(prompt_bufnr)
+        local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if not vim.tbl_isempty(multi) then
+          require('telescope.actions').close(prompt_bufnr)
+          for _, j in pairs(multi) do
+            if j.path ~= nil then
+              vim.cmd(string.format('%s %s', 'edit', j.path))
+            end
+          end
+        else
+          require('telescope.actions').select_default(prompt_bufnr)
+        end
+      end
+      require('telescope').setup({
+          defaults = {
+            mappings = {
+              i = {
+                ['<cr>'] = select_one_or_multi,
+              },
+            },
+          },
+      })
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>f/', builtin.current_buffer_fuzzy_find,
         { desc = '[F]ind lines' })
