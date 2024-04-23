9fe4d85245ab2062d19dc8c1a7ba0269c6e747eb
Jeremiah Griffin <jeremiah@griffin.to>
2024-04-23 16:40:07 -0700

    nvim: Add borders to LSP windows

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -85,6 +85,13 @@ mason_lspconfig.setup_handlers {
   end
 }
 
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = 'rounded',
+})
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = 'rounded',
+})
+
 ------------------------------------------------------------------------------
 -- Options
 
