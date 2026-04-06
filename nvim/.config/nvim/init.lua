1735b483150c3e5349074aacf00ff85d8982d7e6
Jeremiah Griffin <jeremiah@griffin.to>
2026-04-06 13:53:51 -0700

    nvim: Add ui2 configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,7 @@ require("config.diagnostic")
 require("config.lsp")
 require("config.mappings")
 require("config.options")
+require("config.ui2")
 
 -- Load user/init.lua if it exists
 pcall(require, "user.init")
